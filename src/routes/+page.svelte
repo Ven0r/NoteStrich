d295bb321a62f8b5b0faa6e86c4ce162f2304b16
venor <venor.stdout@gmail.com>
2024-08-12 01:05:20 -0500

    Responsive coming soon page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="min-h-screen flex items-center justify-center bg-slate-800 text-white">
   <div class="text-center">
-    <h1 class="text-9xl text-purple-600" style="font-family: 'Ostrich Sans', sans-serif;">Notestrich</h1>
-    <p class="text-xl">Coming Soon</p>
+    <h1 class="text-6xl md:text-8xl lg:text-9xl text-purple-600" style="font-family: 'Ostrich Sans', sans-serif;">Notestrich</h1>
+    <p class="text-lg md:text-xl">Coming Soon</p>
   </div>
 </div>
